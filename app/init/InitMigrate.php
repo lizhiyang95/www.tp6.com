d970d35037f27752a0220c24ef76d14b36d794f6
lizhiyang <2458288906@qq.com>
2023-07-22 12:34:54 +0800

    项目初始化进度2

--- app/init/InitMigrate.php
+++ app/init/InitMigrate.php
@@ -2,12 +2,21 @@
 
 namespace app\init;
 
+use think\App;
+
 class InitMigrate
 {
     public static function init()
     {
-        $dir = root_path();
-        $res = exec("php $dir migrate:run");
-        echo self::class . "初始化结果" . $res;
+        $dir = (new App())->getRootPath();
+//        $res = exec("php {$dir}think migrate:run");
+//        $res = shell_exec("date");
+
+        echo "开始";
+        $cmd = " php {$dir}think migrate:run";
+        echo $cmd;
+//        $res = shell_exec($cmd);
+//        echo $res;
+//        echo self::class . "初始化结果" . $res;
     }
 }
